26c7340b4f0a690f245ce9dad9b5336a15f37ae2
Mark Lee <mlcy0508@gmail.com>
2026-04-27 17:07:03 +0800

    feat: Update colorscheme

--- lua/matugen.lua
+++ lua/matugen.lua
@@ -3,24 +3,24 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#070722", -- Default Background
-    base01 = "#11112d", -- Lighter Background (status bars)
-    base02 = "#17173c", -- Selection Background
-    base03 = "#4e4ec2", -- Comments, Invisibles
+    base00 = "#081512", -- Default Background
+    base01 = "#0f251f", -- Lighter Background (status bars)
+    base02 = "#15342b", -- Selection Background
+    base03 = "#1e6d5a", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#7c80b4", -- Dark Foreground (status bars)
-    base05 = "#f3edf7", -- Default Foreground
-    base06 = "#f3edf7", -- Light Foreground
-    base07 = "#f3edf7", -- Lightest Foreground
+    base04 = "#99a8a4", -- Dark Foreground (status bars)
+    base05 = "#a6b5b1", -- Default Foreground
+    base06 = "#a6b5b1", -- Light Foreground
+    base07 = "#a6b5b1", -- Lightest Foreground
     -- Accent colors
-    base08 = "#fd4663", -- Variables, XML Tags, Errors
-    base09 = "#9bfece", -- Integers, Constants
-    base0A = "#a9aefe", -- Classes, Search Background
-    base0B = "#fff59b", -- Strings, Diff Inserted
-    base0C = "#81fec1", -- Regex, Escape Chars
-    base0D = "#fff280", -- Functions, Methods
-    base0E = "#8188fe", -- Keywords, Storage
-    base0F = "#900017", -- Deprecated, Embedded Tags
+    base08 = "#933636", -- Variables, XML Tags, Errors
+    base09 = "#26a589", -- Integers, Constants
+    base0A = "#167a63", -- Classes, Search Background
+    base0B = "#1e9177", -- Strings, Diff Inserted
+    base0C = "#96e9d6", -- Regex, Escape Chars
+    base0D = "#95e9d6", -- Functions, Methods
+    base0E = "#93ecd7", -- Keywords, Storage
+    base0F = "#3e0f0f", -- Deprecated, Embedded Tags
   })
 end
 
@@ -31,6 +31,7 @@ signal:start(
   vim.schedule_wrap(function()
     package.loaded["matugen"] = nil
     require("matugen").setup()
+    require("lualine").refresh()
   end)
 )
 
