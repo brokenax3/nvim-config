d2eca367aa87118a055094c22dd265a8793e9b54
Mark Lee <markle@cisco.com>
2026-05-21 10:26:15 +0800

    update dotfiles

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,14 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  opts = {
-	  options = { theme = "kanagawa-paper-ink" },
-  },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
+        options = {
+            theme = "kanagawa-paper-ink",
+            disabled_filetypes = {
+                statusline = { "snacks_picker_input", "snacks_explorer" },
+                winbar = { "snacks_picker_input", "snacks_explorer" },
+            },
+        },
+    },
 }
